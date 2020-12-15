e8764cb90524c6edb6819bf1783392f710792877
bgamboe <37429521+bgamboe@users.noreply.github.com>
2020-12-15 17:46:01 -0500

    Add files via upload

---
+++ ServerList.lua
@@ -0,0 +1,5 @@
+return {
+    {Name = "South Asia #1", Address = "172.105.43.77"},
+    {Name = "US East #1", Address = "104.236.19.49"}
+	{Name = "US East #1", Address = "bgamboe-aw.duckdns.org"}
+}
